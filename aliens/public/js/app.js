038914555efe91f8f470023f625af4595aeb3d86
Brooke <Brooke@Mac.local>
2018-11-16 17:39:02 -0500

    adding app.js

---
+++ aliens/public/js/app.js
@@ -0,0 +1,12 @@
+class App extends React.Component {
+  render () {
+    return (
+      <div className='section'>
+      <h1 className='title'>Alien Department of Spaceships</h1>
+      <div className='columns'>
+        <Aliens />
+        </div>
+      </div>
+    )
+  }
+}
