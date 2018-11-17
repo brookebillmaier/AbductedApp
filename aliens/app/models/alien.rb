ac58e0ec7440f8d9efbd1ba63b1f40a269b29c31
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2018-11-17 15:32:31 -0500

    edit function working , need to be on edit button click not on alien click

--- aliens/app/models/alien.rb
+++ aliens/app/models/alien.rb
@@ -89,12 +89,12 @@ class Alien
         SQL
       )
       return {
-          "id" => result["id"].to_i,
-          "name"=> result["name"],
-          "birthday" => result["birthday"],
-          "species" => result["species"],
-          "planet" => result["planet"],
-          "image" => result["image"],
+          "id" => results.first["id"].to_i,
+          "name"=> results.first["name"],
+          "birthday" => results.first["birthday"],
+          "species" => results.first["species"],
+          "planet" => results.first["planet"],
+          "image" => results.first["image"],
       }
     end
 
