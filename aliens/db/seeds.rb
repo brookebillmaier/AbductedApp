c68f72bd94b6b09ca092b54ce225f81ae188119b
Brooke <Brooke@Mac.local>
2018-11-18 11:44:23 -0500

    statements and db

--- aliens/db/seeds.rb
+++ aliens/db/seeds.rb
@@ -6,4 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-aliens = Alien.create([{name:'Dilbert', birthday:'12/1/1900', species:'Martian', planet:'Mars', image:'https://www.colourbox.de/preview/8565283-.jpg'}])
+# aliens = Alien.create([{name:'Dilbert', birthday:'12/1/1900', species:'Martian', planet:'Mars', image:'https://www.colourbox.de/preview/8565283-.jpg'}])
+
+
+# CREATE TABLE aliens (id SERIAL, name VARCHAR(200), birthday VARCHAR(200), species VARCHAR(150), planet VARCHAR(100), image VARCHAR(100));
+
+
+#variables
+
+image = ["https://robohash.org/97.87.2sdf8/?set=set2", "https://robohash.org/raobfdos8/?set=set2", "https://robohash.org/9robotfdhat8/?set=set2","https://robohash.org/alien/?set=set2", "https://robohash.org/alienguy/?set=set2", "https://robohash.org/randoalien/?set=set2", "https://robohash.org/839282.2819382/?set=set2", "https://robohash.org/rsdjkflsajf/?set=set2"]
+
+
+#aliens
+
+10.times do
+  image = image.sample
+  Alien.create({
+    'name' => "#{Faker::FunnyName.name_with_initial}",
+    'birthday' => "#{Faker::Date.birthday(200, 2000)}",
+    'species' => "#{Faker::HitchhikersGuideToTheGalaxy.specie}",
+    'planet' => "#{Faker::HitchhikersGuideToTheGalaxy.planet}",
+    'image' => image
+    })
+end
+p database seeded
--- aliens/public/js/app.js
+++ aliens/public/js/app.js
@@ -2,6 +2,7 @@ class App extends React.Component {
   render () {
     return (
       <div className='section'>
+      <nav></nav>
       <h1 className='title'>Alien Department of Spaceships</h1>
       <div className='columns'>
         <Aliens />
