7ffa62b68322adf63fe5232e1bb5e21236e28539
Brooke <Brooke@Mac.local>
2018-11-19 11:26:44 -0500

    css changes to alien table

--- aliens/public/js/AliensList.js
+++ aliens/public/js/AliensList.js
@@ -1,28 +1,30 @@
 class AliensList extends React.Component {
   render (){
     return (
-      <table>
+      <table className='AliensList'>
         <tbody>
         {this.props.aliens.map((alien, index) => {
           return (
+
             <tr >
               <td onClick={()=> { this.props.getAlien(alien); this.props.toggleState('aliensListIsVisible', 'alienIsVisible')}}>
                 <img src={alien.image} alt={alien.name} className='avatar' />
-                </td>
+              </td>
+
               <td>
                 <h3> {alien.name} </h3>
               </td>
+
               <td>
               <h3>
                 {alien.planet}
               </h3>
               </td>
+
               <td>
-                  <button className='delete' onClick={() => this.props.deleteAlien(alien, index)}>Delete</button>
+                  <button className='delete' onClick={() => this.props.deleteAlien(alien, index)}>Remove</button>
               </td>
-
-              </tr>
-
+            </tr>
           )
         })}
         </tbody>
